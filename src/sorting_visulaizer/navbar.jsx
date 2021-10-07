e7ccd5917779d87a6d4ef1c80080559301d6a398
unbeat01 <unbeatablesinha01@gmail.com>
2021-10-07 14:56:32 +0530

    adding

--- src/sorting_visulaizer/navbar.jsx
+++ src/sorting_visulaizer/navbar.jsx
@@ -47,10 +47,11 @@ class Nav extends Component {
             </div>
 
             <div className='sorting'>
-                <button className={`buttons ${this.state.string === 'mergesort' ? 'lvl' : ''}`} onClick={() => this.mergesort()} >Merge sort</button>
+
                 <button className={`buttons ${this.state.string === 'bubblesort' ? 'lvl' : ''}`} onClick={() => this.bubblesort()}>Bubble sort</button>
                 <button className={`buttons ${this.state.string === 'selectionsort' ? 'lvl' : ''}`} onClick={() => this.selectionsort()}>Selection sort</button>
                 <button className={`buttons ${this.state.string === 'insertionsort' ? 'lvl' : ''}`} onClick={() => this.insertionsort()}>Insertion sort</button>
+                <button className={`buttons ${this.state.string === 'mergesort' ? 'lvl' : ''}`} onClick={() => this.mergesort()} >Merge sort</button>
                 <button className={`buttons ${this.state.string === 'quicksort' ? 'lvl' : ''}`} onClick={() => this.quicksort()}>Quick sort</button>
                 <button className={`buttons ${this.state.string === 'heapsort' ? 'lvl' : ''}`} onClick={() => this.heapsort()}>Heap sort</button>
                 <a className='github' href="https://github.com/unbeat01"><img src={logo} className='logo' alt='profile' /></a>
